2e06296b4d5411897655ebdb1ad2d9d0f6ce13b7
Carlos Villalpando, Jr <carlosvillalpandojr21@yahoo.com>
2019-10-16 16:19:18 -0500

    MemberForm and changeHandler function complete

--- team-builder/src/components/MemberForm.jsx
+++ team-builder/src/components/MemberForm.jsx
@@ -2,10 +2,41 @@ import React, { useState } from 'react';
 
 export const MemberForm = (props) => {
     const [member, setMember] = useState({id: '', name: '', email: '', role: ''})
+
+    const memberChangeHandler = event => {
+        setMember({...member, [event.target.name] : event.target.value})
+        console.log(event.target.name, event.target.value)
+    }
     return (
         <div>
             <form>
-
+                <label htmlFor='name'>Name</label>
+                    <input
+                    name='name'
+                    id='name'
+                    type='text'
+                    placeholder='Member Name'
+                    onChange={memberChangeHandler}
+                    value={member.name}
+                    />
+                <label htmlFor='email'>Email</label>
+                    <input
+                    name='email'
+                    id='email'
+                    type='text'
+                    placeholder='Enter Email'
+                    value={member.email}
+                    onChange={memberChangeHandler}
+                    />
+                <label htmlFor='role'>Role</label>
+                    <input 
+                    name='role'
+                    id='role'
+                    type='text'
+                    placeholder='Enter Role'
+                    value={member.role}
+                    onChange={memberChangeHandler}
+                    />
             </form>
         </div>
     )
